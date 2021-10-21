17949bab0274275e62a27e1ac04f9d206b2190f4
Jack Janes <jackjanes@kevins-mbp.myfiosgateway.com>
2021-10-20 23:56:10 -0400

    paper

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -6,6 +6,9 @@ import SpeedDialAction from '@mui/material/SpeedDialAction';
 import FaceIcon from '@mui/icons-material/Face';
 import SchoolIcon from '@mui/icons-material/School';
 import WorkIcon from '@mui/icons-material/Work';
+import Paper from '@mui/material/Paper';
+import Zoom from '@mui/material/Zoom';
+
 
 const actions = [
   { icon: <FaceIcon />, name: 'Me' },
@@ -13,22 +16,51 @@ const actions = [
   { icon: <WorkIcon />, name: 'Work' },
 ];
 
-export default function BasicSpeedDial() {
-  return (
-    <Box sx={{ height: 320, flexGrow: 1 }}>
-      <SpeedDial
-        ariaLabel="SpeedDial basic example"
-        sx={{ position: 'absolute', bottom: 15, right: 15 }}
-        icon={<SpeedDialIcon />}
-      >
-        {actions.map((action) => (
-          <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-          />
-        ))}
-      </SpeedDial>
+const paper = (
+  <Paper sx={{ position: 'relative', left: 30, top: 30, height: 400, width: 500 }} elevation={22}>
+    <Box component="svg" sx={{ width: 100 }}>
+      Lorem Ipsum
     </Box>
-  );
+  </Paper>
+);
+
+
+class Homepage extends React.Component {
+
+
+
+  render() {
+    return(
+      // <FormControlLabel
+      //   control={<Switch checked={checked} onChange={handleChange} />}
+      //   label="Show"
+      // />
+      //
+      // <Box sx={{ display: 'flex' }}>
+      //   <Zoom in={checked}>{icon}</Zoom>
+      //   <Zoom in={checked} style={{ transitionDelay: checked ? '500ms' : '0ms' }}>
+      //     {icon}
+      //   </Zoom>
+      // </Box>
+      //
+      <Box sx={{}}>
+        {paper}
+        <SpeedDial
+          ariaLabel="SpeedDial basic example"
+          sx={{ position: 'absolute', bottom: 15, right: 15 }}
+          icon={<SpeedDialIcon />}
+        >
+          {actions.map((action) => (
+            <SpeedDialAction
+              key={action.name}
+              icon={action.icon}
+              tooltipTitle={action.name}
+            />
+          ))}
+        </SpeedDial>
+      </Box>);
+  }
+
 }
+
+export default Homepage;
